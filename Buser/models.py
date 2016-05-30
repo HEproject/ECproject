731fabbcb1e2c8a417dc7c5adda4a40b302e4e0c
qq76632253 <76632253@163.com>
2016-05-30 10:44:48 +0800

    lib3

--- Buser/models.py
+++ Buser/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Subject(models.Model):
-    uid = models.IntegerField(default=0, unique=True)
-    name = models.CharField(max_length=20, unique=True)
-    secret = models.CharField(max_length=20)
+    user_id = models.IntegerField(default=0, unique=True)
+    user_name = models.CharField(max_length=20, unique=True)
+    user_passwd = models.CharField(max_length=20)
     sex = models.IntegerField(1)
-    realname = models.CharField(max_length=20)
-    date = models.DateField(auto_now_add=True)
+    real_name = models.CharField(max_length=20)
+    register_date = models.DateField(auto_now_add=True)
     province = models.CharField(max_length=20)
     town = models.CharField(max_length=20)
     area = models.CharField(max_length=20)
-    adress = models.CharField(max_length=20)
+    address = models.CharField(max_length=20)
     
 
     def __unicode__(self):  
