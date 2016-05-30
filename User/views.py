bb7bd956bee640ed430b6092313463e1c373cef6
qq76632253 <76632253@163.com>
2016-05-30 17:32:21 +0800

    create index.html

--- User/views.py
+++ User/views.py
@@ -4,6 +4,10 @@ from django.http import HttpResponse
 
 def index(request):
     context_dict = {'boldmessage': "I am bold font from the context"}
-    return render(request, 'login.html', context_dict)
+    return render(request, 'page/index.html', context_dict)
+
+def login(request):
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render(request, 'page/login.html', context_dict)
 
 # Create your views here.
