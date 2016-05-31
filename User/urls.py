ed4cad44977d318dec105bae29f66e33fe05ce78
qq76632253 <76632253@163.com>
2016-05-31 19:11:11 +0800

    oh no~

--- User/urls.py
+++ User/urls.py
@@ -3,5 +3,9 @@ from User import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^index', views.index, name='index'),
     url(r'^login', views.login, name='login'),
+    url(r'^test', views.test, name='test'),
+    url(r'^register', views.register, name='register'),
+    url(r'^base', views.base, name='base'),
 )
--- User/views.py
+++ User/views.py
@@ -10,4 +10,16 @@ def login(request):
     context_dict = {'boldmessage': "I am bold font from the context"}
     return render(request, 'page/login.html', context_dict)
 
+def test(request):
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render(request, 'page/test.html', context_dict)
+
+def register(request):
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render(request, 'page/register.html', context_dict)
+
+def base(request):
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render(request, 'page/base.html', context_dict)
+
 # Create your views here.
